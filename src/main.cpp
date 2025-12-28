043daa480d9e2423b6c43e643a8e4661dcfb8b32
giovanni B.S <giovannibsonaglio@gmail.com>
2025-12-28 20:37:42 +0100

    feat: added basic behavior to open file and get its content

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        std::cerr << "Please provide a .ced file to compile. Ex: " << argv[0] << " <path_to_file.ced>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    
+    if(!file.good()) {
+        std::cerr << "Could not open the provided file: " << argv[1] << std::endl;
+        return 1;
+    }
+    
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+    std::string file_content = buffer.str();
+	std::cout << file_content << std::endl;
+
     return 0;
 }
